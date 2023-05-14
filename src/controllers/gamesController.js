24b874d9f174efb1e5cbdfa5c26e7061a26cec56
Davi Barci <davibarci0208@gmail.com>
2023-05-14 02:10:08 -0300

    feat: add insert game

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -8,3 +8,21 @@ export async function getGames(req, res) {
       res.status(500).send(err.message);
     }
 }
+
+export async function postGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
+    try {
+      const game = await db.query(`SELECT * FROM games WHERE name=$1;`,[name]);
+      if(game.rowCount) return res.sendStatus(409);
+
+      await db.query( `INSERT INTO games ("name","image","stockTotal", "pricePerDay") 
+        VALUES ($1, $2, $3, $4);`,
+        [name, image, stockTotal, pricePerDay]
+      );
+      
+      res.sendStatus(201);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,8 +1,11 @@
 import {Router} from 'express';
-import { getGames } from '../controllers/gamesController.js';
+import { getGames, postGame } from '../controllers/gamesController.js';
+import { validateSchema } from '../middlewares/validateSchemaMiddleware.js';
+import { gameSchema } from '../schemas/gamesSchema.js';
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
+gamesRouter.post("/games", validateSchema(gameSchema), postGame);
 
 export default gamesRouter;
