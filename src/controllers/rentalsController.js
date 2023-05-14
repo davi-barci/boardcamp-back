b7dd30fb8b0c07612f6430af1aabc2d3bc25ebfd
Davi Barci <davibarci0208@gmail.com>
2023-05-14 17:05:40 -0300

    feat: insert a rent

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,4 +1,5 @@
 import { db } from '../database/database.connection.js';
+import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
     try {
@@ -21,5 +22,33 @@ export async function getRentals(req, res) {
         res.send(obj);
     } catch (err) {
       res.status(500).send(err.message);
+    }
+}
+
+export async function postRentals(req, res){
+    const {customerId, gameId, daysRented } = req.body;
+
+    try{
+        if (daysRented <= 0) res.sendStatus(400);
+
+        const rental = await db.query(`
+        INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+        SELECT $1, $2, $3, $4, $5, 
+            (SELECT "pricePerDay" FROM games WHERE games.id = $2)*$4, 
+            $6
+        WHERE EXISTS (
+            SELECT 1 FROM games
+            WHERE id = $2 AND "stockTotal" > (SELECT COUNT(*) FROM rentals WHERE "gameId" = $2)
+        )
+        AND EXISTS (
+            SELECT 1 FROM customers
+            WHERE id = $1
+        );        
+        `, [customerId, gameId, dayjs().format("YYYY-MM-DD"), daysRented, null, null])
+
+        if(!rental.rowCount) return res.sendStatus(400);
+        res.sendStatus(200);
+    }catch (err){
+        res.status(500).send(err.message);
     }
 }
